4586564eab9bf872b5c59d03e6ae9337f7ce40c9
forestvov1 <kukva2019@gmail.com>
2023-07-10 21:53:51 +1000

    add loader, add not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -2,21 +2,22 @@ import { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import { routeConfig } from 'shared/config/routerConfig/routerConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 export const AppRouter = () => (
-    <Suspense fallback={<div>load</div>}>
-        <Routes>
-            {Object.values(routeConfig).map(({ element, path }) => (
-                <Route
-                    key={path}
-                    path={path}
-                    element={(
+    <Routes>
+        {Object.values(routeConfig).map(({ element, path }) => (
+            <Route
+                key={path}
+                path={path}
+                element={(
+                    <Suspense fallback={<PageLoader />}>
                         <div className="page-wrapper">
                             {element}
                         </div>
-                    )}
-                />
-            ))}
-        </Routes>
-    </Suspense>
+                    </Suspense>
+                )}
+            />
+        ))}
+    </Routes>
 );
---
+++ src/shared/ui/Loader/Loader.tsx
@@ -0,0 +1,21 @@
+import { classNames } from 'shared/lib/classNames/classNames';
+
+import s from './Loader.module.scss';
+
+interface LoaderProps {
+    className?: string
+}
+
+export const Loader = ({ className }: LoaderProps) => (
+    <div className={classNames(s.icon, {}, [className])}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
