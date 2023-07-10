83e3bfe98e506b36d055ab7228bdf2b0a82ccaa6
forestvov1 <kukva2019@gmail.com>
2023-07-10 22:17:25 +1000

    add Error Boundary

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,13 @@
 import { type FC } from 'react';
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage: FC = () => {
     const { t } = useTranslation('main');
 
     return (
         <div>
-            {t('xeeer')}
+            <BugButton />
             {t('Главная страница')}
         </div>
     );
